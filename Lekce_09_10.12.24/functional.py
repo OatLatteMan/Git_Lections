d90a09261e650efd3b371530c014fc75ed7a4fe7
Dmitrii Panov <15minstan@gmail.com>
2024-12-12 17:27:46 +0100

    functional.py correction

--- Lekce_09_10.12.24/functional.py
+++ Lekce_09_10.12.24/functional.py
@@ -1,9 +1,17 @@
 # function.py
 
-data = ['Jan', 'Petr', 'Nina']
+data = ['Petr', 'Jan', 'Nina']
 
-data.sort(key=len)
+def my_func(x):
+    return x[-1]
+
+data.sort(key=len) # řazení podle počtu znaků
 print(data)
 
-# data.sort(key=lambda: )
+data.sort(key=lambda x: x[-1]) # řazení podle posledního znaknu
 print(data)
+
+data.sort(key=lambda x: (len(x), x)) # řazení podle početu a abc
+print(data)
+
+
