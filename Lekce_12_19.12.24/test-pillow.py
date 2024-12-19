fc42528acbc5aad779c3c592c601d9f742d75729
Dmitrii Panov <15minstan@gmail.com>
2024-12-19 19:13:13 +0100

    thursday's lection

---
+++ Lekce_12_19.12.24/test-pillow.py
@@ -0,0 +1,18 @@
+from PIL import Image
+import os
+
+image = Image.open(r"C:\Users\15min\Desktop\DimaFront\Lekce Python\Git_Lections\Lekce_12_19.12.24\witcher4.webp")
+
+new_image = image.resize((960, 540))
+new_image = new_image.rotate(90)
+new_image = new_image.convert('L')
+
+# new_image.show()
+
+images_dir = r"C:\Users\15min\Desktop\DimaFront\Lekce Python\Git_Lections\Lekce_12_19.12.24"
+
+for name in os.listdir(images_dir):
+    if name.endswith(('.jpg', '.webp')):
+        image_path = os.path.join(images_dir, name)
+        print(image_path)
+        print(os.path.isfile(image_path))
