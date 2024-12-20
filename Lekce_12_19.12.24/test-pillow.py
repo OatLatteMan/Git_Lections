4930ec31cdc757a16753bde1fe237e7d0dcb4e78
Dmitrii Panov <15minstan@gmail.com>
2024-12-20 19:31:54 +0100

    pillow library

--- Lekce_12_19.12.24/test-pillow.py
+++ Lekce_12_19.12.24/test-pillow.py
@@ -1,7 +1,7 @@
 from PIL import Image
 import os
 
-image = Image.open(r"C:\Users\15min\Desktop\DimaFront\Lekce Python\Git_Lections\Lekce_12_19.12.24\witcher4.webp")
+image = Image.open(r"C:\Users\15min\Desktop\DimaFront\Lekce Python\Git_Lections\Lekce_12_19.12.24\witcher3.webp")
 
 new_image = image.resize((960, 540))
 new_image = new_image.rotate(90)
@@ -20,9 +20,9 @@ for name in os.listdir(images_dir):
         image = Image.open(image_path)
         print(image.size)
 
-        # if image.size[0] < 346:
-        #     os.remove(image_path)
+        if image.size[0] < 346:
+            os.remove(image_path)
 
-        # print("=======")
-        # print(image_path)
-        # print(image.size)
+        print("=======")
+        print(image_path)
+        print(image.size)
