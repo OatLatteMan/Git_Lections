a60edc6fd8005c32e7189abbb3610435ec8f90f2
Dmitrii Panov <15minstan@gmail.com>
2025-01-14 20:55:14 +0100

    automation testing practice

--- Lekce_15_14.01.25/geometrie.py
+++ Lekce_15_14.01.25/geometrie.py
@@ -4,3 +4,13 @@ def get_triangle_perimeter(a, b , c):
     result = a + b + c
 
     return result
+
+def is_valid_triangle(a, b, c):
+    data = [a, b, c]
+    data.sort()
+    return (data[0] + data[1]) > data[2]
+
+def get_square_area(a):
+    if a <= 0:
+        raise ValueError('Strana A musi byt kladna')
+    return a*a
--- Lekce_15_14.01.25/geometrie_test.py
+++ Lekce_15_14.01.25/geometrie_test.py
@@ -12,8 +12,29 @@ class Test_geometrie(unittest.TestCase):
         result = g.get_triangle_perimeter(10, 40, 33)
         self.assertEqual(result, 83)
 
-    def test_triangle_validity(self):
-        pass
+    def test_is_valid_triangle_10_10_10(self):
+        result = g.is_valid_triangle(10, 10, 10)
+        self.assertTrue(result)
+
+    def test_is_valid_triangle_10_10_100(self):
+        result = g.is_valid_triangle(10, 10, 100)
+        self.assertFalse(result)
+
+    def test_isvalid_triangle_10_10_n10(self):
+        result = g.is_valid_triangle(10, 10, -10)
+        self.assertFalse(result)
+
+    def test_get_square_area_10(self):
+        result = g.get_square_area(10)
+        self.assertEqual(result, 100)
+
+    def test_get_square_area_1000(self):
+        result = g.get_square_area(1000)
+        self.assertEqual(result, 1000000)
+
+    def test_get_square_area_n10(self):
+        with self.assertRaises(ValueError):
+            g.get_square_area(-10)
 
     def test_get_circle_perimeter(self):
         pass
