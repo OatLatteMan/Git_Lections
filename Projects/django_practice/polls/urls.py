8b876e8a30053e9e603e84f401e4102109a09b43
Dmitrii Panov <15minstan@gmail.com>
2025-02-02 01:09:00 +0100

    django tutorial practice part 3

--- Projects/django_practice/polls/urls.py
+++ Projects/django_practice/polls/urls.py
@@ -2,6 +2,19 @@ from django.urls import path
 from polls import views
 
 urlpatterns = [
-    path('index/', views.index, name='index'),
+    # http://127.0.0.1:2091/polls/tester
+    path('indexes/', views.indexes, name='indexes'),
+
+    # http://127.0.0.1:2091/polls/
+    path('', views.index, name='index'),
+
+    # http://127.0.0.1:2091/5/
+    path("<int:question_id>/", views.detail, name="detail"),
+
+    # http://127.0.0.1:2091/5/results/
+    path("<int:question_id>/results/", views.results, name="results"),
+
+    # http://127.0.0.1:2091/5/vote
+    path("<int:question_id>/vote/", views.vote, name="vote"),
 ]
 
