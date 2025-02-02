d32539a640146dd0b5623aa878c28920896d47f8
Dmitrii Panov <15minstan@gmail.com>
2025-02-02 01:13:08 +0100

    django tutorial practice part 3

--- Projects/django_practice/polls/views.py
+++ Projects/django_practice/polls/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Question
+from django.template import loader
+
+def index(request):
+    latest_question_list = Question.objects.order_by("-pub_date")[:5]
+    context = {"latest_question_list": latest_question_list}
+    return render(request, "polls/index.html", context)
 
 def indexes(request):
     return HttpResponse('Greetings for Dima')
@@ -14,9 +20,4 @@ def results(request, question_id):
 
 def vote(request, question_id):
     return HttpResponse("You're voting on question %s." % question_id)
-
-def index(request):
-    latest_question_list = Question.objects.order_by("-pub_date")[:5]
-    output = ", ".join([q.question_text for q in latest_question_list])
-    return HttpResponse(output)
 
