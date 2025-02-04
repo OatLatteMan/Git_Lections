96b762b859e6999064d7d9b003187bdd6842259c
Dmitrii Panov <15minstan@gmail.com>
2025-02-04 20:12:17 +0100

    django practice lection

--- Projects/test_project/todo/models.py
+++ Projects/test_project/todo/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from datetime import datetime
+from django.utils import timezone
 
-# Create your models here.
+class Task(models.Model):
+    title = models.CharField(max_length=100)
+    desc = models.CharField(max_length=500)
+    create_dt = models.DateTimeField(auto_now_add=True)
+    update_dt = models.DateTimeField(auto_now=True)
+
