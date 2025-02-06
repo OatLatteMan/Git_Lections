8373bf69b196851c8e3239d90a1f66015a04249f
Dmitrii Panov <15minstan@gmail.com>
2025-02-06 20:50:22 +0100

    django practice lections

--- Projects/test_project/todo/views.py
+++ Projects/test_project/todo/views.py
@@ -1,17 +1,47 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from todo.models import Task
+from django.forms import ModelForm
+
+class TaskForm(ModelForm):
+    class Meta:
+        model = Task
+        fields = ['title', 'desc']
 
 def index(request):
     return render(request, 'todo/index.html')
 
 def todo_list(request):
-    return render(request, 'todo/list.html')
+    tasks = Task.objects.all()[0:100]
+
+    context = {
+        'tasks': tasks
+    }
+
+    return render(request, 'todo/list.html', context)
 
 def todo_new(request):
-    return render(request, 'todo/new.html')
+    form = TaskForm()
+    context = {
+        'form': form,
+    }
+
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            task = form.save()
+            print(task, task.id)
+            return redirect('/todo/list.html')
+    else:
+        form = TaskForm()
+        return render(request, 'todo/new.html', context)
+
 
 def todo_detail(request, number):
+    task = Task.objects.get(id=number)
+
     context = {
+        'task': task,
         'number': number,
     }
 
