6afcf1a30a26f1de705c2dd1b90f3e962a7789fd
Dmitrii Panov <15minstan@gmail.com>
2025-02-07 23:59:29 +0100

    django practice tutorial part 7

--- Projects/test_project/final_project/views.py
+++ Projects/test_project/final_project/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+# function index()
 def index(request, number):
     context = {
         'number': number,
     }
 
     return render(request, 'final_project/tpcfuj.html', context)
+
