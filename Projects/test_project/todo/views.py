7bc6751010ebf0da4ea56f559e46f8bc5ba79e1c
Dmitrii Panov <15minstan@gmail.com>
2025-02-12 23:58:38 +0100

    films and serials final project

--- Projects/test_project/todo/views.py
+++ Projects/test_project/todo/views.py
@@ -2,11 +2,16 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from todo.models import Task
 from django.forms import ModelForm
+from django import forms
 
 class TaskForm(ModelForm):
     class Meta:
         model = Task
         fields = ['title', 'desc']
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'input'}),
+            'desc': forms.Textarea(attrs={'class': 'textarea'}),
+        }
 
 def index(request):
     return render(request, 'todo/index.html')
@@ -21,11 +26,6 @@ def todo_list(request):
     return render(request, 'todo/list.html', context)
 
 def todo_new(request):
-    form = TaskForm()
-    context = {
-        'form': form,
-    }
-
     if request.method == 'POST':
         form = TaskForm(request.POST)
         if form.is_valid():
@@ -34,10 +34,10 @@ def todo_new(request):
             return redirect('/todo/list')
         else:
             form = TaskForm(request.POST)
-            return render(request, 'todo/new.html', context)
+            return render(request, 'todo/new.html', {'form': form})
     else:
         form = TaskForm()
-        return render(request, 'todo/new.html', context)
+        return render(request, 'todo/new.html', {'form': form})
 
 
 def todo_detail(request, number):
