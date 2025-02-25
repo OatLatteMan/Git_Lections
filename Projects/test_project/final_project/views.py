74fb8229b50528fd2d8cb4e25563b94ceff3b007
Dmitrii Panov <15minstan@gmail.com>
2025-02-26 00:40:22 +0100

    final_project

--- Projects/test_project/final_project/views.py
+++ Projects/test_project/final_project/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse
 from django.contrib.auth.models import User
-
+from final_project.models import ItemType, Genre, Item, Review
 
 """
 Tabs:
