e8e3866d3bcd023be47a0fce1cecce95c417e634
Dmitrii Panov <15minstan@gmail.com>
2025-03-04 20:38:27 +0100

    it school

--- Projects/it_school/school/models.py
+++ Projects/it_school/school/models.py
@@ -1,4 +1,15 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
 
 class Course(models.Model):
-    name = models.CharField(max)
+    name = models.CharField(max_length=100)
+    desc = models.CharField(max_length=1000)
+    image = models.ImageField(blank=True, null=True, upload_to='course/')
+
+    lector = models.ForeignKey(User, )
+
+    def __str__(self):
+        return self.name
