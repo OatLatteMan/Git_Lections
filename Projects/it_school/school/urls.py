f24f29d5707658cf1dcfb3bcf9c1e8baa3c73637
Dmitrii Panov <15minstan@gmail.com>
2025-03-06 18:54:42 +0100

    static files

--- Projects/it_school/school/urls.py
+++ Projects/it_school/school/urls.py
@@ -1,8 +1,8 @@
-from django.contrib import admin
 from django.urls import path
+from school import views
 
 app_name = 'school'
 
 urlpatterns = [
-
+    path('index/', views.index, name='index')
 ]
